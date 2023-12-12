be05ed10aff9e8c9badcb5f403bb9eb8da5eb15a
harisptrtm <harisptrtm@gmail.com>
2023-12-12 16:07:35 +0700

    double scroll bar fix

--- src/app/page.js
+++ src/app/page.js
@@ -50,7 +50,7 @@ export default function Index() {
           <div class="path"></div>
           <div class="d-flex">
             <div class="about">
-              <h2 class="text-center">
+              <h2 class="text-center color-green">
                 Tentang Kami
                 <div class="accessories-1">
                   <svg xmlns="http://www.w3.org/2000/svg" width="95" height="95" viewBox="0 0 95 95" fill="none">
