47f9c47c54d6bcf1ad79026bd29d32cbc5fae000
harisptrtm <harisptrtm@gmail.com>
2024-02-19 14:17:15 +0700

    Tentang Kami Mobile

--- src/app/page.js
+++ src/app/page.js
@@ -265,6 +265,25 @@ export default function Index() {
           </div>
         </div>
       </div>
+      <div class="mobile-content">
+        <div class="about">
+          <h2 class={`${futuraMD.className} text-lg-center color-green`}>
+            Tentang Kami
+            <div class="accessories-1">
+              <svg xmlns="http://www.w3.org/2000/svg" width="95" height="95" viewBox="0 0 95 95" fill="none">
+                <line x1="19.5" y1="47" x2="19.5" y2="15" stroke="#879679" stroke-width="3"/>
+                <line x1="5.60728" y1="48.5571" x2="1.60728" y2="38.5571" stroke="#879679" stroke-width="3"/>
+                <line x1="80" y1="93.5" x2="69" y2="93.5" stroke="#879679" stroke-width="3"/>
+                <line x1="36.5604" y1="46.5787" x2="48.5604" y2="5.57865" stroke="#879679" stroke-width="3"/>
+                <line x1="50.6888" y1="55.2715" x2="80.6888" y2="1.27153" stroke="#879679" stroke-width="3"/>
+                <line x1="68.483" y1="78.139" x2="94.483" y2="68.5919" stroke="#879679" stroke-width="3"/>
+                <line x1="62.9793" y1="63.9008" x2="90.9793" y2="37.9008" stroke="#879679" stroke-width="3"/>
+              </svg>
+            </div>
+          </h2>
+          <p><b class="color-green">PAPILARS</b> adalah <b class="color-green">Digital Creative Agency</b> yang menggabungkan <b class="color-green">Keahlian dan Teknologi.</b> Kombinasi tim dengan berbagai latar belakang menciptakan nilai tinggi dalam solusi layanan yang diimplementasikan. Layanan kami mencakup <b class="color-green">Desain dan Manajemen dalam ranah solusi kreatif, pemasaran, dan branding.</b></p>
+        </div>
+      </div>
       {/* <div class="main-content">
         <div class="gradient-overlay"></div>
         <spline-viewer url="https://prod.spline.design/NcZZD82sxOjmIaXw/scene.splinecode"></spline-viewer>
